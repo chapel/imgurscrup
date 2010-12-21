bc582af29b4b2c599e939e012daf5fad84260678
Jacob Chapel <jacob.chapel@gmail.com>
2010-12-21 06:43:48 -0800

    Added regexp to strip / from querystring

--- server.js
+++ server.js
@@ -4,34 +4,40 @@ var http = require('http'),
     
     
 var server = http.createServer(function(req, res) {
-  var url = req.url.replace('/', '')
+  var url = req.url.replace(/[^a-zA-Z 0-9=]+/g, '')
   var query = querystring.parse(url)
-  var request = client.request('POST', '/2/upload.json?key='+query.key, {
-    'Host':'api.imgur.com',
-    'Content-Type': 'image/png',
-    'Transfer-Encoding': 'chunked'
-  })
-  
-  req.on('data', function(chunk) {
-    request.write(chunk)
-  })
+  if (!query.key) {
+    res.writeHead(500, {"content-Type": "text/plain"})
+    res.end('URL should end with key=<key> where <key> is your Imgur.com Anonymous API key.')
+  } else {
+    var request = client.request('POST', '/2/upload.json?key='+query.key, {
+      'Host':'api.imgur.com',
+      'Content-Type': 'image/png',
+      'Transfer-Encoding': 'chunked'
+    })
 
-  req.on('end', function() {
-    request.end()
-    request.on('response', function(response) {
-      var result = ''
-      response.on('data', function(chunk) {
-        result += chunk
-      })
-      response.on('end', function() {
-        var temp = JSON.parse(result), url = ''
-        if (temp.error) url = temp.error.message
-        else url = temp.upload.links.original
-        res.writeHead(201, {"Content-Type": "text/plain", 'Content-Length': url.length})
-        res.end(url)
+    req.on('data', function(chunk) {
+      request.write(chunk)
+    })
+
+    req.on('end', function() {
+      request.end()
+      request.on('response', function(response) {
+        var result = ''
+        response.on('data', function(chunk) {
+          result += chunk
+        })
+        response.on('end', function() {
+          var temp = JSON.parse(result), url = ''
+          if (temp.error) url = temp.error.message
+          else url = temp.upload.links.original
+          res.writeHead(201, {"Content-Type": "text/plain", 'Content-Length': url.length})
+          res.end(url)
+        })
       })
     })
-  })
+  }
 })
 
-server.listen(80)
+// Change this number to change what port is used. e.g. http://127.0.0.1:8000/key=<key>
+server.listen(8000)
